47344006af23351d787e0bd97a78e8d66261945d
Abhinav Singh <284509091+abhinavsingh25039-glitch@users.noreply.github.com>
2026-06-10 15:16:37 +0530

    Setup Node.js backend and gitignore

--- server.js
+++ server.js
@@ -60,3 +60,15 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend Started Successfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
